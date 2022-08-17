36f0d7cb9ff1c5eef744434c3454e21ff63991c1
frsiam <siam.frs@gmail.com>
2022-08-17 06:13:23 +0600

    using swiper js for banner

--- src/Components/Pages/Home/Banner.js
+++ src/Components/Pages/Home/Banner.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const Banner = () => {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import required modules
+import { Navigation } from "swiper";
+
+export default function Banner() {
     return (
-        <div>
-            <h1>top banner</h1>
-        </div>
+        <>
+            <h1>Banner</h1>
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={2}
+                onSlideChange={
+                    () => console.log('slide change')
+                }
+                onSwiper={
+                    (swiper) => console.log(swiper)
+                }
+            >
+                <SwiperSlide>
+                    <img src="https://i.ibb.co/YdSPC8F/Strawberry.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://i.ibb.co/BB69hfC/mandarin.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                ...
+            </Swiper>
+        </>
     );
-};
-
-export default Banner;
+}
